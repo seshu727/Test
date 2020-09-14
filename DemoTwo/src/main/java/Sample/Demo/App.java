6a522b4daa9aadc7d49993e2ef2f7faa763bc9d6
seseshu <pseshu48@gmail.com>
2020-09-14 17:49:33 +0530

    done

--- DemoTwo/src/main/java/Sample/Demo/App.java
+++ DemoTwo/src/main/java/Sample/Demo/App.java
@@ -1,9 +1,25 @@
 package Sample.Demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	protected static WebDriver driver;
+
+	@BeforeTest
+	public static void setupp() throws Throwable {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\seshu\\Desktop\\pit issues\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+	}
+
+    @Test   
+	public void name() {
+		System.out.println("done");
+	}
 }
